b232ebfe302313aca6cb5c523c01521802ed31a9
jahotor <87078051+jahotor@users.noreply.github.com>
2022-11-09 19:30:15 +0000

    re commited files

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema( {
